db4cb69c913ec83f0be0e15f9f63d0765d512f8e
tigranesIIthegreat <tigransuqiasyan.2001@gmail.com>
2023-11-10 23:24:16 +0400

    improve parameter names for ALU module

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,32 +1,33 @@
-module alu(input [7:0] A,B,
-           input [3:0] Sel,
-           output [7:0] Out,
-           output CarryOut);
+module alu(input [7:0] operand1,
+           input [7:0] operand2,
+           input [3:0] opCode,
+           output [7:0] result,
+           output carryOut);
 
     reg [7:0] ALU_Result;
     wire [8:0] tmp;
-    assign Out = ALU_Result;
-    assign tmp = {1'b0,A} + {1'b0,B};
-    assign CarryOut = tmp[8];
+    assign result = ALU_Result;
+    assign tmp = {1'b0,operand1} + {1'b0,operand2};
+    assign carryOut = tmp[8];
     always @(*) begin
-        case(Sel)
-            4'b0000: ALU_Result = A + B ;           // Addition 
-            4'b0001: ALU_Result = A - B ;           // Subtraction
-            4'b0010: ALU_Result = A * B;            // Multiplication
-            4'b0011: ALU_Result = A/B;              // Division
-            4'b0100: ALU_Result = A<<1;             // Logical shift left
-            4'b0101: ALU_Result = A>>1;             // Logical shift right
-            4'b0110: ALU_Result = {A[6:0],A[7]};    // Rotate left
-            4'b0111: ALU_Result = {A[0],A[7:1]};    // Rotate right
-            4'b1000: ALU_Result = A & B;            // Logical and 
-            4'b1001: ALU_Result = A | B;            // Logical or
-            4'b1010: ALU_Result = A ^ B;            // Logical xor 
-            4'b1011: ALU_Result = ~(A | B);         // Logical nor
-            4'b1100: ALU_Result = ~(A & B);         // Logical nand 
-            4'b1101: ALU_Result = ~(A ^ B);         // Logical xnor
-            4'b1110: ALU_Result = (A>B)?8'd1:8'd0;  // Greater comparison
-            4'b1111: ALU_Result = (A==B)?8'd1:8'd0; // Equal comparison   
-            default: ALU_Result = A + B;
+        case(opCode)
+            4'b0000: ALU_Result = operand1 + operand2 ;             // Addition 
+            4'b0001: ALU_Result = operand1 - operand2 ;             // Subtraction
+            4'b0010: ALU_Result = operand1 * operand2;              // Multiplication
+            4'b0011: ALU_Result = operand1/operand2;                // Division
+            4'b0100: ALU_Result = operand1<<1;                      // Logical shift left
+            4'b0101: ALU_Result = operand1>>1;                      // Logical shift right
+            4'b0110: ALU_Result = {operand1[6:0],operand1[7]};      // Rotate left
+            4'b0111: ALU_Result = {operand1[0],operand1[7:1]};      // Rotate right
+            4'b1000: ALU_Result = operand1 & operand2;              // Logical and 
+            4'b1001: ALU_Result = operand1 | operand2;              // Logical or
+            4'b1010: ALU_Result = operand1 ^ operand2;              // Logical xor 
+            4'b1011: ALU_Result = ~(operand1 | operand2);           // Logical nor
+            4'b1100: ALU_Result = ~(operand1 & operand2);           // Logical nand 
+            4'b1101: ALU_Result = ~(operand1 ^ operand2);           // Logical xnor
+            4'b1110: ALU_Result = (operand1>operand2)?8'd1:8'd0;    // Greater comparison
+            4'b1111: ALU_Result = (operand1==operand2)?8'd1:8'd0;   // Equal comparison   
+            default: ALU_Result = operand1 + operand2;
         endcase
     end
 endmodule
--- test/alu.v
+++ test/alu.v
@@ -4,19 +4,19 @@ module alu_tb;
     parameter DATA_WIDTH = 8;
     parameter SEL_WIDTH = 4;
 
-    reg [DATA_WIDTH - 1 : 0] A;
-    reg [DATA_WIDTH - 1 : 0] B;
-    reg [SEL_WIDTH - 1 : 0] Sel;
+    reg [DATA_WIDTH - 1 : 0] operand1;
+    reg [DATA_WIDTH - 1 : 0] operand2;
+    reg [SEL_WIDTH - 1 : 0] opCode;
 
-    wire [DATA_WIDTH-1:0] Out;
-    wire CarryOut;
+    wire [DATA_WIDTH-1:0] result;
+    wire carryOut;
 
     alu ALU_UUT (
-        .A(A),
-        .B(B),
-        .Sel(Sel),
-        .Out(Out),
-        .CarryOut(CarryOut)
+        .operand1(operand1),
+        .operand2(operand2),
+        .opCode(opCode),
+        .result(result),
+        .carryOut(carryOut)
     );
 
     reg clk = 0;
@@ -30,7 +30,7 @@ module alu_tb;
     initial begin
         log_file = $fopen("log/core/alu.log", "w");
         $fdisplay(log_file, 
-            "A\t\t\tB\t\t\tSel\t\tOut\t\t\tCarryOut\n----------------------------------------------------");
+            "operand1\t\t\tB\t\t\tSel\t\tOut\t\t\tCarryOut\n----------------------------------------------------");
 
         #10 reset = 1;
         #10 reset = 0;
@@ -44,34 +44,34 @@ module alu_tb;
     end
 
     initial begin
-        A = 0;
-        B = 0;
-        Sel = 0;
+        operand1 = 0;
+        operand2 = 0;
+        opCode = 0;
 
         repeat (2**DATA_WIDTH * 2**DATA_WIDTH * 2**SEL_WIDTH) begin
             @(posedge clk); // Wait for next clock cycle (if using synchronous design)
 
             // Log the inputs and outputs
-            $fdisplay(log_file, "%b\t%b\t%b\t%b\t%b", A, B, Sel, Out, CarryOut);
+            $fdisplay(log_file, "%b\t%b\t%b\t%b\t%b", operand1, operand2, opCode, result, carryOut);
 
             // Increment inputs for the next test
-            if (B == (2**DATA_WIDTH - 1)) begin
-                B = 0;
-                if (A == (2**DATA_WIDTH - 1)) begin
-                    A = 0;
-                    if (Sel == (2**SEL_WIDTH - 1)) begin
-                        Sel = 0;
+            if (operand2 == (2**DATA_WIDTH - 1)) begin
+                operand2 = 0;
+                if (operand1 == (2**DATA_WIDTH - 1)) begin
+                    operand1 = 0;
+                    if (opCode == (2**SEL_WIDTH - 1)) begin
+                        opCode = 0;
                     end
                     else begin
-                        Sel = Sel + 1;
+                        opCode = opCode + 1;
                     end
                 end
                 else begin
-                    A = A + 1;
+                    operand1 = operand1 + 1;
                 end
             end
             else begin
-                B = B + 1;
+                operand2 = operand2 + 1;
             end
         end
 
