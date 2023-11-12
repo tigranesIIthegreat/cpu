ecba4ba44a24d217af50e16fba671a66287e106f
tigranesIIthegreat <tigransuqiasyan.2001@gmail.com>
2023-11-12 13:21:10 +0400

    improve logging for compiling and running

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from colorama import Fore, Style
 
 class Path(str):
     TEST = 'test'
@@ -14,15 +15,15 @@ class Extension(str):
 script_directory = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(script_directory)
 
-def corresponding_path(path, root):
+def _corresponding_path(path, root):
     path_from_project = os.path.relpath(path, project_root)
     relpath = path_from_project.split('/', 1)[1] if len(path_from_project) == 1 else path_from_project
     return os.path.join(root, relpath)
 
-def change_extension(file_path, new_extension):
+def _change_extension(file_path, new_extension):
     return os.path.splitext(file_path)[0] + new_extension
 
-def generate_log_files():
+def _generate_log_files():
     test_root = os.path.join(project_root, Path.TEST)
     log_root = os.path.join(project_root, Path.LOG)
     for root, _, files in os.walk(test_root):
@@ -30,15 +31,29 @@ def generate_log_files():
             if not file.endswith(Extension.TEST): 
                 continue
             file = os.path.relpath(os.path.join(root, file), test_root)
-            log_file = change_extension(corresponding_path(file, log_root), Extension.LOG)
+            log_file = _change_extension(_corresponding_path(file, log_root), Extension.LOG)
             log_file_dir = os.path.dirname(log_file)
             if not os.path.exists(log_file_dir): 
                 os.makedirs(log_file_dir)
             if not os.path.exists(log_file): 
                 open(log_file, 'w').close()
 
+def _run_command(command: list):
+    print()
+    print(f"{Fore.WHITE + Style.BRIGHT}COMMAND :{Style.RESET_ALL}", ' '.join(command))
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if result.stdout:
+        print(f"{Fore.WHITE + Style.BRIGHT} STDOUT :{Style.RESET_ALL}", result.stdout, end='')
+    if result.stderr:
+        print(f"{Fore.WHITE + Style.BRIGHT} STDERR :{Style.RESET_ALL}", result.stderr, end='')
+
+    print(f"{Fore.WHITE + Style.BRIGHT} STATUS :", end='')
+    print(f"{Fore.RED} FAIL" if result.returncode else f"{Fore.GREEN} SUCCESS")
+    print(f"{Style.RESET_ALL}")
+
 
 def compile():
+    _generate_log_files()
     test_root = os.path.join(project_root, Path.TEST)
     vvp_root = os.path.join(project_root, Path.VVP)
     for root, _, files in os.walk(test_root):
@@ -46,13 +61,13 @@ def compile():
             if not file.endswith(Extension.TEST): 
                 continue
             file = os.path.relpath(os.path.join(root, file), test_root)
-            vvp_file = change_extension(corresponding_path(file, vvp_root), Extension.VVP)
+            vvp_file = _change_extension(_corresponding_path(file, vvp_root), Extension.VVP)
             vvp_file_dir = os.path.dirname(vvp_file)
             if not os.path.exists(vvp_file_dir): 
                 os.makedirs(vvp_file_dir)
             if not os.path.exists(vvp_file): 
                 open(vvp_file, 'w').close()
-            subprocess.run(['iverilog', '-o', vvp_file, os.path.join(test_root, file)])
+            _run_command(['iverilog', '-o', vvp_file, os.path.join(test_root, file)])
 
 def run():
     vvp_abs_path = os.path.join(project_root, Path.VVP)
@@ -61,9 +76,8 @@ def run():
             if not file.endswith('.vvp'):
                 continue
             vvp_file_path = os.path.join(root, file)
-            command = ['vvp', vvp_file_path]
-            subprocess.run(command)
-            
-generate_log_files()
-compile()
-run()
+            _run_command(['vvp', vvp_file_path])
+
+if __name__ == '__main__':     
+    compile()
+    run()
